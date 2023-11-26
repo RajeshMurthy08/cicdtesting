3676fc03cb848f518dd5203fadad92bfaec510db
RajeshMurthy08 <rajeshnurthy06@gmail.com>
2023-11-26 12:59:02 +0530

    pom.xml added

---
+++ src/Interview.java
@@ -0,0 +1,19 @@
+
+public class Interview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		String[] a={"Rrrrrrrr"};
+		String b="";
+		for(int i=0;i<a.length-1;i++) {
+			if(a[i].contains("p")) {
+				
+				count++;
+				System.out.println(count);
+			}
+		}
+
+	}
+
+}
